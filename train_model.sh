168ba0e2c46fc3c92d667a789611d4139f7311d9
Jason Lin <jasonlin90125@gmail.com>
2025-04-01 17:01:59 -0400

    Slurm for train

--- train_model.sh
+++ train_model.sh
@@ -3,17 +3,17 @@
 #SBATCH --output=edm_train.out
 #SBATCH --error=edm_train.err
 #SBATCH -p volta-gpu
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:4
 #SBATCH -n 1
 #SBATCH --cpus-per-task=8
-#SBATCH --time=3-00:00:00
+#SBATCH --time=5-00:00:00
 #SBATCH --mem=32G
 #SBATCH --qos=gpu_access
 #SBATCH --mail-type=end
 #SBATCH --mail-user=shuhang@unc.edu
 
 # Activate conda environment
-source $HOME/anaconda3/etc/profile.d/conda.sh
+source $HOME/miniconda3/etc/profile.d/conda.sh
 conda activate diffusion
 
 module load cuda/12.5
@@ -22,17 +22,19 @@ echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 nvidia-smi --query-gpu=memory.total,memory.used --format=csv
 
 python main_water.py \
-    --n_epochs 3000 \
+    --n_epochs 1000 \
     --exp_name edm_water \
     --n_stability_samples 1000 \
     --diffusion_noise_schedule polynomial_2 \
     --diffusion_noise_precision 1e-5 \
     --diffusion_steps 1000 \
     --diffusion_loss_type l2 \
-    --batch_size 64 \
+    --batch_size 128 \
     --nf 256 \
     --n_layers 9 \
+    --n_report_steps 100 \
     --lr 1e-4 \
     --normalize_factors [1,4,10] \
     --test_epochs 20 \
-    --ema_decay 0.9999
+    --ema_decay 0.9999 \
+    --no_wandb
